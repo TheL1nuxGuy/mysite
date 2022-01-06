d84bfeedaed9837044f94d23e24c158b2b978fcc
tlg <sloprominecraft@gmail.com>
2022-01-06 22:03:43 +0100

    fix

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function klik() {
   x.style.color = "red";
 }
 
-function ke() {
+function ka() {
   document.body.style.backgroundImage =
     "url('https://www.google.com/url?sa=i&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3ASolid_black.svg&psig=AOvVaw1f1kIZMdRDkQHn-52VgAXy&ust=1641589185566000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCKi53baCnvUCFQAAAAAdAAAAABAD')";
 }
