928c14a19208f1a4a2cdf46f11241bb12ce1784d
tlg <sloprominecraft@gmail.com>
2022-01-07 03:32:58 +0100

    js add alert + right click alert.

--- script.js
+++ script.js
@@ -12,3 +12,15 @@ function ka() {
   x.style.color = "blue"
   x.style.fontFamily = "monospace";
 }
+alert("Welcome to my site")
+document.onmousedown=click
+var times=0
+var times2=10
+function click() {
+if ((event.button==2) || (event.button==3)) {
+if (times>=1) { bye() }
+alert("No right clicking!!!!!! don't do it again..");
+times++ } }
+function bye() {
+alert("I said NO right clicking! click ok to exit LMAO!");
+bye() }
